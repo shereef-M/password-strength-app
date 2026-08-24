c505fe37ed2028bbd8df168a3d04df2d42c672a3
Abubakar Muhammad Sherif <abubakarsherrif2@gmail.com>
2026-08-24 21:25:33 +0100

    Fix Inngest client to use cloud mode in production

--- server/inngest/client.js
+++ server/inngest/client.js
@@ -2,7 +2,7 @@ const { Inngest } = require("inngest");
 
 const inngest = new Inngest({
   id: "passguard-api",
-  isDev: true,
+  isDev: process.env.NODE_ENV !== "production",
 });
 
 module.exports = { inngest };
